059804aadc2a32799c09a28a588857586d3b25f4
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-06-17 16:34:10 +0530

    make it better

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ nltk.download('stopwords')
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('wordnet')
-nltk.download('omw-1.4')
+# nltk.download('omw-1.4')
 
 class SentimentRecommenderModel:
 
